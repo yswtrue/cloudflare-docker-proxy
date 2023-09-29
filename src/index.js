31180c24b8d16f1f7a464081b0771efe76549ec5
roy <a365039311@gmail.com>
2023-09-29 11:17:24 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,8 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "gitlab-registry.yswtrue.com": "https://registry.gitlab.com",
+  "docker-registry.yswtrue.com": "https://registry-1.docker.io",
 };
 
 function routeByHosts(host) {
